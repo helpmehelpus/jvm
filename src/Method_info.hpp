8426e037b6ae3fdf7e83527a449ca3f0b64ac001
Iure Vieira Brandão <iurebrandao@gmail.com>
2018-07-05 23:18:08 -0300

    doxygen

--- src/Method_info.hpp
+++ src/Method_info.hpp
@@ -19,9 +19,28 @@ public:
     unsigned char attributes_count;
     vector <Attribute_info> attributes;
 
-    
+    /*!
+    * Ler os métodos do .class
+    * @param fp ponteiro para o arquivo .class
+    * @param cp_vector Vetor para armazenamento de dados do ConstantPool.
+    * @param lenght Número de métodos a serem lidos
+	* @return retorna um vetor de métodos
+    */
     static vector <Method_info> read_methods(FILE*, vector<Cp_info>, int);
+    
+    /*!
+    * Ler um método específico
+    * @param fp ponteiro para o arquivo .class
+    * @param cp_vector Vetor para armazenamento de dados do ConstantPool.
+	* @return retorna o método
+    */
     static Method_info read_method(FILE*, vector<Cp_info>);
+
+    /*!
+    * Pega as flags de um método
+    * @param flags flag a ser análisada para retornar o tipo do método
+	* @return retorna uma string com as flags
+    */
     static string get_method_flags(unsigned short flags);
 };
 
--- src/Operand_stack.hpp
+++ src/Operand_stack.hpp
@@ -35,25 +35,97 @@ public:
 	bool typed_element_pushed;
 	static const bool bits64 = (sizeof(int*) == 8);
 	const int real_max_size;
+	/*!
+    * Pega o tipo da variável que está no topo da pilha
+	* @return retorna o elemento
+    */
 	uint8_t top_type();
+	/*!
+    * Pega o elemento que está no topo da pilha \e Element
+	* @return retorna o elemento
+    */
 	Element top_value();
+	/*!
+    * Desempilha o elemento que está na pilha de \e Element
+	* @return retorna o elemento desempilhado
+    */
 	Element pop_element();
+	/*!
+    * Desempilha o elemento que está na pilha de \e Typed_element
+	* @return retorna o elemento desempilhado
+    */
 	Typed_element pop_typed_element();
+	/*!
+    * Acrescenta um inteiro ao topo da pilha, utilizando 2 slots
+	* @param x inteiro a ser empilhado 
+    */
 	void push_type(int);
+	/*!
+    * Acrescenta um inteiro ao topo da pilha
+	* @param x inteiro a ser empilhado 
+    */
     void push_type(long);
+	/*!
+    * Acrescenta um long ao topo da pilha, utilizando 2 slots
+	* @param x long a ser empilhado 
+    */
     void push_type(float);
+	/*!
+    * Acrescenta um float ao topo da pilha
+	* @param x float a ser empilhado 
+    */
 	void push_type(double);
+	/*!
+    * Acrescenta um double ao topo da pilha, utilizando 2 slots
+	* @param x double a ser empilhado 
+    */
 	void push_type(bool);
+	/*!
+    * Acrescenta um bool ao topo da pilha
+	* @param x bool a ser empilhado 
+    */
 	void push_type(int*);
+	
+	/*!
+    * Acrescenta um elemento ao topo da pilha
+	* @param typed_element elemento a ser empilhado 
+    */
 	void push_type(Typed_element);
+	
+	/*!
+    * Acrescenta um elemento ao topo da pilha, utilizando 2 slots
+	* @param element elemento a ser empilhado 
+    */
 	void push_type(Element, uint8_t);
+	
+	/*!
+    * Acrescenta um elemento ao topo da pilha, utilizando 2 slots
+	* @param x elemento a ser empilhado 
+	* @param tipo tipo do elemento a ser empilhado
+    */
 	void push(Element x, uint8_t tipo);
+	
 	int size();
 
+	/*!
+    * Retorna a string com os valores da pilha de operandos
+	* @return string com os valores da pilha  
+    */
     string get_string();
+	
+	/*!
+    * Debuga a pilha de operandos
+	* 
+    */
 	void debug_operand_stack();
 	
+	/*!
+    * Verifica se a pilha de operandos está vazia
+	* @return retorna um booleano se a pilha está vazia
+	* 
+    */
 	bool empty();
+	
 	void printALL();
 	const int max_size;
 };
